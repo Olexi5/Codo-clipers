60c715018b960ed2c4dbff5743d5698864f15bb8
Olexi5 <sashkov2012@gmail.com>
2025-12-13 09:21:35 +0000

    Deploying to gh-pages from @ Olexi5/Codo-clipers@9ef89a3b22bdef0d5aeb3bf3b46dd91d98b6e841 🚀

---
+++ index.6a7dc6e4.js
@@ -0,0 +1,2 @@
+!function(){var o={openMobBtn:document.querySelector("[data-mob-open]"),closeMobBtn:document.querySelector("[data-mob-close]"),mob:document.querySelector("[data-mob]")};function e(){o.mob.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}o.openMobBtn.addEventListener("click",e),o.closeMobBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.6a7dc6e4.js.map
